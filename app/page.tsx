1f366a919fa3aac4e1093376ce5f83cd4d09506c
Allen John <allenjohnmonapallil@gmail.com>
2024-08-23 16:57:58 +0530

    new components and functions

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,9 @@ import React from 'react'
 import Guest from '@/components/Guest'
 import { currentUser } from '@clerk/nextjs/server'
 import AddTransaction from '@/components/AddTransaction'
+import Balance from '@/components/Balance'
+import IncomeExpense from '@/components/IncomeExpense'
+import TransactionList from '@/components/TransactionList'
 
 const HomePage = async () => {
   const user = await currentUser()
@@ -12,8 +15,11 @@ const HomePage = async () => {
 
   return (
     <main>
-      <h1>Welcome, {user?.firstName}</h1>
+      <h2>Welcome, {user?.firstName}</h2>
+      <Balance/>
+      <IncomeExpense/>
       <AddTransaction/>
+      <TransactionList />
     </main>
   )
 }
